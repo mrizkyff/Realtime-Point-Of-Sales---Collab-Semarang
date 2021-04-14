149e87c632d23dfcbf9dcd16438516c726155d04
mrizkyff <muhamadrizkyff@gmail.com>
2021-04-14 12:13:20 +0700

    login as tenant not user

--- application/models/M_Login.php
+++ application/models/M_Login.php
@@ -3,19 +3,19 @@
     {
         public function cek_login($data){
             $this->db->where($data);
-            return $this->db->get('tb_user');
+            return $this->db->get('tb_tenant');
         }
         public function find_id($data){
             $this->db->where($data);
-            $this->db->select('id_user');
-            return $this->db->get('tb_user')->result();
+            $this->db->select('id');
+            return $this->db->get('tb_tenant')->result();
         }
         public function find_level($data){
             $this->db->where($data);
             $this->db->select('level');
             $this->db->select('foto');
             $this->db->select('status');
-            return $this->db->get('tb_user')->result();
+            return $this->db->get('tb_tenant')->result();
         }
         
     }
